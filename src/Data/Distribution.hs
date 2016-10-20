773ba1403ccbfc9578ae067576d451d744ab26a6
Rob Rix <rob.rix@github.com>
2016-10-20 13:27:55 -0400

    Variables.

--- src/Data/Distribution.hs
+++ src/Data/Distribution.hs
@@ -5,3 +5,10 @@ import System.Random
 data Expr a where
   StdRandom :: Random a => Expr a
   Lit :: a -> Expr a
+  Get :: Var a -> Expr a
+  Let :: Var a -> Expr a -> Expr b -> Expr b
+
+data Var a where
+  Double :: String -> Var Double
+  Bool :: String -> Var Bool
+  Int :: String -> Var Int
