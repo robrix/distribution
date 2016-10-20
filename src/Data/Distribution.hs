e8a579963e958ea8c5a7d348d66ea40290775e95
Rob Rix <rob.rix@github.com>
2016-10-20 13:28:54 -0400

    Add a Num instance.

--- src/Data/Distribution.hs
+++ src/Data/Distribution.hs
@@ -8,8 +8,22 @@ data Expr a where
   Get :: Var a -> Expr a
   Let :: Var a -> Expr a -> Expr b -> Expr b
   Not :: Expr Bool -> Expr Bool
+  Neg :: Num a => Expr a -> Expr a
+  Abs :: Num a => Expr a -> Expr a
+  Sig :: Num a => Expr a -> Expr a
+  Add :: Num a => Expr a -> Expr a -> Expr a
+  Mul :: Num a => Expr a -> Expr a -> Expr a
 
 data Var a where
   Double :: String -> Var Double
   Bool :: String -> Var Bool
   Int :: String -> Var Int
+
+
+instance Num a => Num (Expr a) where
+  (+) = Add
+  (*) = Mul
+  abs = Abs
+  signum = Sig
+  fromInteger = Lit . fromInteger
+  negate = Neg
